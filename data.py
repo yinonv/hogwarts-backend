0eb8612916b502336463b5f66f03d207126ed476
Yinon Vahab <yinonv@gmail.com>
2020-01-11 22:53:23 +0200

    first commit

---
+++ data.py
@@ -0,0 +1,12 @@
+student_list = [{"existing":[{"skill":"Animation","level":"2"},{"skill":"Elemental","level":"5"}],"desired":[{"skill":"Self-detonation","level":"4"}],"courses":["Magic for day-to-day life"],"name":"Yinon","lastName":"Vahab","createDate":{"date":"10/1/20","time":"12:10"},"id":1},
+{"existing":[{"skill":"Summoning","level":"1"},{"skill":"Water breathing","level":"1"}],"desired":[{"skill":"Possession","level":"1"},{"skill":"Animation","level":"3"}],"courses":["Magic for medical professionals","Alchemy basics"],"name":"Gabe","lastName":"Gordon","createDate":{"date":"10/1/20","time":"12:12"},"id": 2}
+]
+
+
+skills_list = ['Alchemy', 'Animation', 'Conjuror', 'Disintegration', 'Elemental', 'Healing', 'Illusion', 'Immortality'
+, 'Invisibility', 'Invulnerability', 'Necromancer', 'Omnipresent', 'Omniscient', 'Poison', 'Possession'
+, 'Self-detonation', 'Summoning', 'Water breathing']
+
+courses_list = ["Alchemy basics", "Alchemy advanced", "Magic for day-to-day life",
+ "Magic for medical professionals", "Dating with magic"]
+
---
+++ main.py
@@ -0,0 +1,39 @@
+from flask import Flask, request, jsonify
+import json
+from flask_cors import CORS
+from data import student_list as students
+from data import skills_list
+from data import courses_list
+
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/students', methods=['GET'])
+def all_students():
+    global students
+    return json.dumps(students)
+
+
+@app.route('/skills', methods=['GET'])
+def skills():
+    return json.dumps(skills_list)
+
+
+@app.route('/courses', methods=['GET'])
+def courses():
+    return json.dumps(courses_list)
+
+
+@app.route('/students/add/', methods=['POST'])
+def new_student():
+    global students
+    student = request.get_json(force=True)
+    students.append(student)
+    return "Added"
+
+
+if __name__ == "__main__":
+    app.run(host='localhost', port=2600)
+
+    
