424a2bf3fff9dc07a2df02861554a8a752cf320c
Yinon Vahab <yinonv@gmail.com>
2020-01-12 00:58:23 +0200

    heroku works

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-import json, os
+import json
 from flask_cors import CORS
 from data import student_list as students
 from data import skills_list
@@ -39,7 +39,6 @@ def new_student():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='127.0.0.1', port=2700)
 
     
